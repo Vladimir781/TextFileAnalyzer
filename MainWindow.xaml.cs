9c50b06dbb927cd48081315448fa9b57c3efe77d
Vladimir Didok <vladimir24863@gmail.com>
2023-06-08 01:22:37 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of OpenFileDialog for selecting the input file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog.Title = "Select the input file";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Get the selected input file path
+                string inputFilePath = openFileDialog.FileName;
+
+                // Create an instance of SaveFileDialog for selecting the output file
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+                saveFileDialog.Title = "Select the output file";
+
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Get the selected output file path
+                    string outputFilePath = saveFileDialog.FileName;
+
+                    // Create an instance of TextFileAnalyzer
+                    TextFileAnalyzer analyzer = new TextFileAnalyzer(inputFilePath);
+
+                    // Analyze the file
+                    analyzer.AnalyzeFile();
+
+                    // Update the TextBox with the analysis result
+                    OutText.Text = $"Total Word Count: {analyzer.TotalWordCount}\n" +
+                                   $"Unique Word Count: {analyzer.UniqueWordCount}\n" +
+                                   $"Total Palindrome Count: {analyzer.TotalPalindromeCount}\n\n" +
+                                   "Word Counts:\n";
+
+                    foreach (var entry in analyzer.wordCounts)
+                    {
+                        OutText.Text += $"{entry.Key} - {entry.Value}\n";
+                    }
+
+                    OutText.Text += "\nPalindrome Counts:\n";
+
+                    foreach (var entry in analyzer.palindromeCounts)
+                    {
+                        OutText.Text += $"{entry.Key} - {entry.Value}\n";
+                    }
+
+                    OutText.Text += "\nPalindrome Sentences:\n";
+
+                    foreach (string sentence in analyzer.palindromeSentences)
+                    {
+                        OutText.Text += $"{sentence}\n";
+                    }
+
+                    // Write the analysis result to the output file
+                    analyzer.WriteAnalysisResult(outputFilePath);
+                }
+
+
+
+                
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ TextFileAnalyzer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    public class TextFileAnalyzer
+    {
+        private string filePath;
+        public Dictionary<string, int> wordCounts;
+        public Dictionary<string, int> palindromeCounts;
+        public List<string> palindromeSentences;
+
+        public int TotalWordCount { get; private set; }
+        public int UniqueWordCount { get; private set; }
+        public int TotalPalindromeCount { get; private set; }
+
+        public TextFileAnalyzer(string filePath)
+        {
+            this.filePath = filePath;
+            this.wordCounts = new Dictionary<string, int>();
+            this.palindromeCounts = new Dictionary<string, int>();
+            this.palindromeSentences = new List<string>();
+        }
+
+        public void AnalyzeFile()
+        {
+            // Read the file and split it into sentences
+            string text = File.ReadAllText(filePath);
+            string[] sentences = text.Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Reset the counts and lists
+            TotalWordCount = 0;
+            UniqueWordCount = 0;
+            TotalPalindromeCount = 0;
+            wordCounts.Clear();
+            palindromeCounts.Clear();
+            palindromeSentences.Clear();
+
+            foreach (string sentence in sentences)
+            {
+                // Split the sentence into words
+                string[] words = sentence.Split(new[] { ' ', '\t', '\n', '\r', ',', ';', '-', '—' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Check if the sentence contains palindromes
+                if (HasPalindrome(words))
+                {
+                    // Add the sentence to the palindrome sentences list
+                    palindromeSentences.Add(sentence);
+                }
+
+                foreach (string word in words)
+                {
+                    // Increment the total word count
+                    TotalWordCount++;
+
+                    // Normalize the word to lowercase
+                    string normalizedWord = word.ToLower();
+
+                    // Count the word occurrences
+                    if (wordCounts.ContainsKey(normalizedWord))
+                        wordCounts[normalizedWord]++;
+                    else
+                    {
+                        wordCounts[normalizedWord] = 1;
+                        UniqueWordCount++;
+                    }
+
+                    // Check if the word is a palindrome
+                    if (IsPalindrome(normalizedWord))
+                    {
+                        // Increment the palindrome count
+                        TotalPalindromeCount++;
+
+                        // Count the palindrome occurrences
+                        if (palindromeCounts.ContainsKey(normalizedWord))
+                            palindromeCounts[normalizedWord]++;
+                        else
+                            palindromeCounts[normalizedWord] = 1;
+                    }
+                }
+            }
+            UniqueWordCount--; // Subtract 1 from UniqueWordCount
+            TotalWordCount--;  // Subtract 1 from TotalWordCount
+        }
+
+
+
+
+
+
+
+
+        public void WriteAnalysisResult(string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                writer.WriteLine("Total Word Count: " + TotalWordCount);
+                writer.WriteLine("Unique Word Count: " + UniqueWordCount);
+                writer.WriteLine("Total Palindrome Count: " + TotalPalindromeCount);
+                writer.WriteLine();
+
+                writer.WriteLine("Word Counts:");
+                foreach (var entry in wordCounts)
+                    writer.WriteLine(entry.Key + " - " + entry.Value);
+
+                writer.WriteLine();
+
+                writer.WriteLine("Palindrome Counts:");
+                foreach (var entry in palindromeCounts)
+                    writer.WriteLine(entry.Key + " - " + entry.Value);
+
+                writer.WriteLine();
+
+                writer.WriteLine("Palindrome Sentences:");
+                foreach (string sentence in palindromeSentences)
+                    writer.WriteLine(sentence);
+            }
+        }
+
+        private bool IsPalindrome(string word)
+        {
+            if (word.Length <= 2)
+                return false;
+
+            int left = 0;
+            int right = word.Length - 1;
+
+            while (left < right)
+            {
+                if (word[left] != word[right])
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        private bool HasPalindrome(string[] words)
+        {
+            foreach (string word in words)
+            {
+                if (IsPalindrome(word.ToLower()))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
+
